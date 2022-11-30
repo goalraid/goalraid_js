7b5e353abb77cbd66fef900b4d7afccf964d2890
goalraid <102480914+goalraid@users.noreply.github.com>
2022-11-30 01:21:17 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 import av
 
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
 class VideoProcessor:
     def recv(self, frame):
         frm = frame.to_ndarray(format="bgr24")
@@ -9,4 +12,11 @@ class VideoProcessor:
         return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
 
-webrtc_streamer(key="key", video_processor_factory=VideoProcessor)
+webrtc_ctx = webrtc_streamer(
+    key="key",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration=RTC_CONFIGURATION,
+    video_processor_factory=VideoProcessor,
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=False,
+)
