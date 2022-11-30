b05ec125fdfa37c95a550864524d58519e98010c
goalraid <102480914+goalraid@users.noreply.github.com>
2022-11-30 01:14:09 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class VideoProcessor:
         frm = frame.to_ndarray(format="bgr24")
 
 
-        return frame.av.VideoFrame.from_ndarray(frm, format='bgr24')
+        return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
 
 webrtc_streamer(key="key", video_processor_factory=VideoProcessor)
