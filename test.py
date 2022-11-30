45586508c23fb5325b8426b69ab72b7670f8184c
goalraid <102480914+goalraid@users.noreply.github.com>
2022-11-30 00:13:19 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+from streamlit_webrtc import webrtc_streamer
+import av
+
+class VideoProcessor:
+    def recv(self, frame):
+        frm = frame.to_ndarray(format="bgr24")
+
+
+        return frame.av.VideoFrame.from_ndarray(frm, format='bgr24')
+
+
+webrtc_streamer(key="key", video_processor_factory=VideoProcessor)
